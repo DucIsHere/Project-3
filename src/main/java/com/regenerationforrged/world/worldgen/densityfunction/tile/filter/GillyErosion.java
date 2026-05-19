5e950d5ae9145f0b9570be858f888c0f01873842
DucIsHere <220372506+DucIsHere@users.noreply.github.com>
2026-05-19 06:04:21 +0000

    :)))

---
+++ src/main/java/com/regenerationforrged/world/worldgen/densityfunction/tile/filter/GillyErosion.java
@@ -0,0 +1,8 @@
+package com.regenerationforrged.world.worldgen.densityfunction.tile.filter;
+
+import java.util.function.IntFunction;
+
+import com.regenerationforrged.world.worldgen.cell.Cell;
+import com.regenerationforrged.world.worldgen.GeneratorContext;
+import com.regenerationforrged.world.worldgen.densityfunction.tile.Tile;
+import com.regenerationforrged.world.worldgen.densityfunction.tile.Size;
