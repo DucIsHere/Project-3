49f024055e209acfa2a692cae73fcc945d2d1743
DucIsReal <220372506+DucIsHere@users.noreply.github.com>
2026-09-11 04:53:51 +0000

    :))))

--- TheProject-C/src/include/math/InterporlationFast.s
+++ TheProject-C/src/include/math/InterporlationFast.s
@@ -6,3 +6,20 @@ testl %edi, %edi
 jz .L_zero
 
 lzcntl %edi, %ecx
+movl %ecx, %eax
+
+subl $16, %eax
+sarl $1, %eax
+
+leal 16(%rax), %ecx
+movl %edi, %eax
+shrv %cl, %eax
+addl $0xC000, %eax
+
+movl $16, %ecx
+subl %eax, %ecx
+ret
+
+.L_zero:
+xorl %eax, %eax
+ret
